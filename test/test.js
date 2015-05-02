5fc43c02f2410cc052d9023dbe9e5ee54bc5b356
Emilio <emilioplatzer@gmail.com>
2015-05-02 18:48:04 -0300

    Agregando casos de prueba

--- test/test.js
+++ test/test.js
@@ -1,11 +1,34 @@
 
 var expect = require('expect.js');
-var sv = require('..');
+var dirInfo = require('..');
 
 describe('dir-info', function(){
-    describe('basic test', function(){
-        it('first test', function(){
-            throw new Error('no tests');
+    describe('accesing the file system', function(){
+        var dirs=[{
+            dir:'simple-git',
+            is:{git:true, github:false, svn:false},
+            status:{unstaged:true, changed:false},
+            server:{outdated:null, origin:null}
+        },{
+            dir:'auto-reference-github',
+            is:{git:true, github:true, svn:false},
+            status:{unstaged:false, changed:true},
+            server:{outdated:true, origin:'https://github.com/codenautas/fast-devel-server.git'}
+        }];
+        var calls=[{
+            opts:{cmd:false, net:false},
+            resultMask:{status:null, server:null}
+        },{
+            opts:{cmd:true, net:false},
+            resultMask:{server:null}
+        }];
+        it('recognizes a git dir', function(done){
+            dirInfo.getInfo('test/fixtures/simple-git').then(function(info){
+                expect(info.is.git).to.be.ok();
+                expect(info.is.github).to.not.be.ok();
+                expect(info.summary).to.eql('g');
+                done();
+            }).catch(done);
         });
     });
 });
