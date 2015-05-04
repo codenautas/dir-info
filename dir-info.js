7d7e2e10b9709cc0f56a5898fe9bff39b5c51c88
Emilio <emilioplatzer@gmail.com>
2015-05-03 23:10:12 -0300

    Agrego pruebas con carpetas comunes y con jsones

--- dir-info.js
+++ dir-info.js
@@ -12,17 +12,39 @@ var Promise = require('promise');
  
 var dirInfo = {}; // this module
 
+dirInfo.summaryTexts = {
+    is:{
+        github:'h',
+        git:'g',
+        svn:'s',
+        multilang:'m',
+        "package.json":'p',
+        json:'j',
+        other:'',
+        unknown:'?'
+    },
+    status:{
+        changed:'C',
+        unstaged:'U',
+        ignored:'i',
+        error:'E', // for json & package.json
+        ok:'',
+        unknown:'?',
+    },
+    server:{
+        outdated:'O',
+        ok:'',
+        unknown:'?',
+    }
+}
+
 dirInfo.getInfo = function getInfo(path, opts){
     return Promise.resolve().then(function(){
         return {
             dir:path, // BAD! only the last dirname
-            is:{
-                git:false,
-                github:false,
-                svn:false,
-            },
-            status:null,
-            server:null
+            is:'unknown',
+            status:'unknown',
+            server:'unknown'
         };
     });
 }
--- test/test.js
+++ test/test.js
@@ -13,16 +13,38 @@ var skip = { // provisional
 }
 
 describe('dir-info', function(){
-    var dirs=[{
+    var paths=[{
         dir:'simple-git',
-        is:{git:true, github:false, svn:false},
-        status:{unstaged:true, changed:false},
-        server:{outdated:null, origin:null}
+        is:'git',
+        status:'unstaged',
+        server:'',
+        origin:'unknown'
     },{
         dir:'auto-reference-github',
-        is:{git:true, github:true, svn:false},
-        status:{unstaged:false, changed:true},
-        server:{outdated:true, origin:'https://github.com/codenautas/fast-devel-server.git'}
+        is:'github',
+        status:'changed', // has priority over unstaged
+        server:'outdated', 
+        origin:'https://github.com/codenautas/fast-devel-server.git'
+    },{
+        dir:'simple-dir',
+        is:'other',
+        status:'ok', 
+        server:''
+    },{
+        dir:'simple-dir/package.json',
+        is:'package.json',
+        status:'ok', 
+        server:''
+    },{
+        dir:'simple-git/other.json',
+        is:'json',
+        status:'error', 
+        server:''
+    },{
+        dir:'simple-dir/package.json',
+        is:'package.json',
+        status:'ok', 
+        server:'outdated' // because mocha version. can use npm-check-updates
     }];
     before(function(done){
         Promise.resolve().then(function(){
@@ -30,8 +52,12 @@ describe('dir-info', function(){
         }).then(function(){
             return fs.copy('./test/fixtures', dirbase, {clobber:true});
         }).then(function(){
-            return Promise.all(dirs.map(function(dir){
-                return fs.rename(dirbase+'/'+dir.dir+'/dot-git',dirbase+'/'+dir.dir+'/.git');
+            return Promise.all(paths.map(function(path){
+                if(path.is.substr(0,3)==='git'){
+                    return fs.rename(dirbase+'/'+path.dir+'/dot-git',dirbase+'/'+path.dir+'/.git');
+                }else{
+                    return Promise.resolve();
+                }
             }));
         }).then(function(){
             done();
@@ -43,18 +69,29 @@ describe('dir-info', function(){
     describe('simple tests', function(){
         it.skip('recognizes a git dir', function(done){
             dirInfo.getInfo(dirbase+'/simple-git').then(function(info){
-                expect(info.is.git).to.be.ok();
-                expect(info.is.github).to.not.be.ok();
-                if(!skip.summary){
-                    expect(info.summary).to.eql('g');
-                }
+                expect(info.is).to.eql('git');
                 done();
             }).catch(done);
         });
         it.skip('recognizes a github dir', function(done){
             dirInfo.getInfo(dirbase+'/auto-reference-github').then(function(info){
-                expect(info.is.git).to.be.ok();
-                expect(info.is.github).to.be.ok();
+                expect(info.is).to.eql('github');
+                done();
+            }).catch(done);
+        });
+        it.skip('run command for get more info', function(done){
+            dirInfo.getInfo(dirbase+'/simple-git',{cmd:true}).then(function(info){
+                expect(info.is).to.eql('git');
+                expect(info.status).to.eql('unstaged');
+                expect(info.server === null).to.be.ok();
+                done();
+            }).catch(done);
+        });
+        it.skip('connect to de net for get more info', function(done){
+            dirInfo.getInfo(dirbase+'/simple-git',{cmd:true}).then(function(info){
+                expect(info.is).to.eql('git');
+                expect(info.status).to.eql('changed');
+                expect(info.server).to.eql('outdated');
                 done();
             }).catch(done);
         });
@@ -67,17 +104,17 @@ describe('dir-info', function(){
             opts:{cmd:true, net:false},
             resultMask:{server:null}
         },{
-            opts:{cmd:true, net:false},
+            opts:{cmd:true, net:true},
             resultMask:{}
         }];
         it.skip('call comprehensive tests', function(done){
-            Promise.all(_.flatten(dirs.map(function(dir){
+            Promise.all(_.flatten(paths.map(function(path){
                 return calls.map(function(call){
-                    return dirInfo.getInfo(dirbase+'/'+dir.dir, call.opts).then(function(info){
-                        var expected = _.merge({}, dir, call.resultMask);
+                    return dirInfo.getInfo(dirbase+'/'+path.dir, call.opts).then(function(info){
+                        var expected = _.merge({}, path, call.resultMask);
                         expect(info).to.eql(expected);
                     }).catch(function(err){
-                        console.log('ERROR in case',dir,call);
+                        console.log('ERROR in case',path,call);
                         console.log(err);
                         throw err;
                     });
