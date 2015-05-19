ac1f894344c412026a4d1d7f7f5d54c9c15f04e1
Emilio <emilioplatzer@gmail.com>
2015-05-19 17:12:06 -0300

    paso vía async y cambio la llamada para usar un objeto con opciones === en vez de ==

--- example/example.js
+++ example/example.js
@@ -7,21 +7,47 @@ var Promise = require('best-promise');
 
 var getInfo;
 
+function getInfoAsync(path, opts, callback){
+    dirInfo.getInfo(path,opts).then(function(result){
+        callback(null, result);
+    }).catch(function(err){
+        callback(err);
+    });
+}
+
+function memoizeePromise2a(f){
+    var memoized = memoizee(f, { async: true });
+    return function(a1, a2){
+        var parameters = arguments;
+        return new Promise(function(resolve, reject){
+            memoized(a1,a2,function(err,res){
+                if(err){
+                    reject(err);
+                }else{
+                    resolve(res);
+                }
+            });
+        });
+    }
+}
+
 // var thePath='../..';
 var thePath='c:/hecho/npm/dir-info/package.json';
 
+var opts={cmd:true, net:true};
+
 Promise.resolve().then(function(){
     console.log('sin memo',moment().format());
-    return dirInfo.getInfo(thePath,{cmd:true, net:true});
+    return dirInfo.getInfo(thePath,opts);
 }).then(function(info){
     console.log('ceropia vez:',info);
     console.log('arranco',moment().format());
-    getInfo = memoizee(dirInfo.getInfo);
-    return getInfo(thePath,{cmd:true, net:true});
+    getInfo = memoizeePromise2a(getInfoAsync);
+    return getInfo(thePath,opts);
 }).then(function(info){
     console.log('primera vez:',info);
     console.log('vamos',moment().format());
-    return getInfo(thePath,{cmd:true, net:true});
+    return getInfo(thePath,opts);
 }).then(function(info){
     console.log('segunda vez:',info);
     console.log('fin',moment().format());
