12cd32f46c2b8a31b79c133e829a06ed1d527e9b
Diego F.(EW7) <diegoefe@gmail.com>
2016-12-27 16:48:59 -0300

    Actualizo a qa-control 0.3.0

--- test/test.js
+++ test/test.js
@@ -3,7 +3,6 @@
 var _ = require('lodash');
 var expect = require('expect.js');
 var dirInfo = require('..');
-// var fsExtra = require('fs-extra');
 var fs = require('fs-promise');
 var expectCalled = require('expect-called');
 var Path = require('path');
